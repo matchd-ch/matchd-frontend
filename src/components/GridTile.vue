c9c4a087fb490517f52bdafe3c0ef30699fd50f9
Guillem Gelabert Sunyer <guillem.gelabert@liip.ch>
2021-04-27 17:26:59 +0200

    MATCHD-278: Extract grid tile to own component

---
+++ src/components/GridTile.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="search-result-grid__image-wrap">
+    <div class="search-result-grid__image-box rounded-full border-2">
+      <img
+        :src="
+          match.img.replace('{stack}', resultType === 'student' ? 'desktop-square' : 'logo-square')
+        "
+        class="w-full"
+        :alt="`${match.name} Logo`"
+      />
+    </div>
+    <div v-if="match.matchStatus?.initiator" class="search-result-grid__match-status-helper">
+      <div class="search-result-grid__match-status">
+        <span v-if="match.matchStatus.confirmed" class="material-icons">people</span>
+        <span v-else class="material-icons">record_voice_over</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, prop } from "vue-class-component";
+
+class Props {
+  match = prop<any>({});
+  resultType = prop<string>({});
+}
+export default class ProfileSection extends Vue.with(Props) {}
+</script>
+
+<style scoped></style>
