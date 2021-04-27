2be27f7b15b1c35bd53baf198d61f4f2796b6f0d
Guillem Gelabert Sunyer <guillem.gelabert@liip.ch>
2021-04-27 16:56:46 +0200

    MATCHD-278: Show list of companies

--- src/views/CompanyList.vue
+++ src/views/CompanyList.vue
@@ -1,9 +1,36 @@
 <template>
   <div>
     <h1>Firmen</h1>
+    <ul>
+      <li v-for="company in companyMatching.data" :key="company.id">{{ company.id }}</li>
+    </ul>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script lang="ts">
+import { ActionTypes } from "@/store/modules/content/action-types";
+import type { Company } from "api";
+import { Vue } from "vue-class-component";
+
+Vue.registerHooks(["beforeRouteUpdate"]);
+
+export default class CompanyList extends Vue {
+  async mounted(): Promise<void> {
+    await this.loadData();
+  }
+
+  get companyMatching(): { data: Company[] } {
+    return this.$store.getters["companyMatching"];
+  }
+
+  async loadData(): Promise<void> {
+    try {
+      await this.$store.dispatch(ActionTypes.COMPANY_MATCHING);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
+</script>
 
 <style scoped></style>
