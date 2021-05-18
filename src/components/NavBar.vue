a92d0b905465878ea9ef392d34a454e2c1000112
Guillem Gelabert Sunyer <guillem.gelabert@liip.ch>
2021-05-18 13:41:42 +0200

    MATCHD-259: Refine styles

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -6,7 +6,7 @@
     <div class="p-4 flex flex-row items-center justify-between">
       <a
         href="#"
-        class="text-lg tracking-widest text-gray-900 uppercase rounded-lg dark-mode:text-white focus:outline-none focus:shadow-outline"
+        class="text-lg tracking-widest text-gray-900 uppercase rounded-lg focus:outline-none focus:shadow-outline"
         >Matchd</a
       >
       <button
@@ -35,14 +35,14 @@
       class="flex-col flex-grow pb-4 md:pb-0 hidden md:flex md:justify-end md:flex-row"
     >
       <slot />
-      <nav-dropdown text="Jeremy">
+      <nav-dropdown :text="userName">
         <a
-          class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+          class="block px-4 py-2 mt-2 text-sm bg-transparent rounded-lg hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
           href="#"
           >Link #1</a
         >
         <a
-          class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+          class="block px-4 py-2 mt-2 text-sm bg-transparent rounded-lg md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
           href="#"
           >Link #2</a
         >
--- src/components/NavDropdown.vue
+++ src/components/NavDropdown.vue
@@ -2,21 +2,9 @@
   <div x-on:click.away="open = false" class="relative" x-data="{ open: false }">
     <button
       x-on:click="open = !open"
-      class="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-left bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:focus:bg-gray-600 dark-mode:hover:bg-gray-600 md:w-auto md:inline md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+      class="flex flex-row items-center w-full rounded-full px-10 py-2 mt-2 text-sm border text-left bg-transparent md:w-auto md:inline md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
     >
       <span>{{ text }}</span>
-      <svg
-        fill="currentColor"
-        viewBox="0 0 20 20"
-        x-class="{ 'rotate-180': open, 'rotate-0': !open }"
-        class="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1"
-      >
-        <path
-          fill-rule="evenodd"
-          d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-          clip-rule="evenodd"
-        ></path>
-      </svg>
     </button>
     <div
       x-show="open"
@@ -28,7 +16,7 @@
       x-transition:leave-end="transform opacity-0 scale-95"
       class="absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg md:w-48"
     >
-      <div class="px-2 py-2 bg-white rounded-md shadow dark-mode:bg-gray-800">
+      <div class="px-2 py-2 bg-white rounded-md shadow">
         <slot />
       </div>
     </div>
