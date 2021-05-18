1a8666334ece2161a1f3f8e94edccaf2ea42a964
Guillem Gelabert Sunyer <guillem.gelabert@liip.ch>
2021-05-18 11:02:45 +0200

    MATCHD-259: Fix styles

--- src/components/NavLink.vue
+++ src/components/NavLink.vue
@@ -1,12 +1,12 @@
 <template>
-  <router-link :to="to">
-    <a
-      class="px-4 py-2 mt-2 text-sm bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-      ><slot
-    /></a>
-  </router-link>
+  <a
+    class="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+  >
+    <router-link :to="to">
+      <slot />
+    </router-link>
+  </a>
 </template>
-
 <script lang="ts">
 import { Vue, prop } from "vue-class-component";
 
