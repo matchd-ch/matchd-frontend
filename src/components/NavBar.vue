fdcf8a62f771d005a55450aef5b8738753b2f848
Guillem Gelabert Sunyer <guillem.gelabert@liip.ch>
2021-05-18 18:59:29 +0200

    MATCHD-259: Add logo

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,14 +1,14 @@
 <template>
   <div
     x-data="{ open: false }"
-    class="flex flex-col px-4 md:items-center md:justify-between md:flex-row md:px-6 lg:px-8 border-green-1 border-b py-2.5"
+    class="flex flex-col md:items-center md:justify-between md:flex-row px-2.5 border-green-1 border-b py-2.5"
   >
     <div class="p-4 flex flex-row items-center justify-between">
       <a
         href="#"
         class="text-lg tracking-widest text-gray-900 uppercase rounded-lg focus:outline-none focus:shadow-outline"
-        >Matchd</a
-      >
+        ><Logo class="w-36"
+      /></a>
       <button
         class="md:hidden rounded-lg focus:outline-none focus:shadow-outline"
         x-on:click="open = !open"
@@ -43,10 +43,11 @@
 import { Options, Vue } from "vue-class-component";
 import NavDropdown from "./NavDropdown.vue";
 import NavLink from "./NavLink.vue";
+import Logo from "@/assets/icons/logo.svg";
 
 @Options({
   name: "NavBar",
-  components: { NavDropdown, NavLink },
+  components: { NavDropdown, NavLink, Logo },
 })
 export default class NavBar extends Vue {}
 </script>
