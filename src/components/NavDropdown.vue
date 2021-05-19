1a8991d8734920a778d59e0be5f0bfe292253efb
Guillem Gelabert Sunyer <guillem.gelabert@liip.ch>
2021-05-19 16:21:00 +0200

    MATCHD-259: Remove alpinejs dependency

--- src/components/NavDropdown.vue
+++ src/components/NavDropdown.vue
@@ -1,30 +1,25 @@
 <template>
-  <div x-on:click.away="open = false" class="relative" x-data="{ open: false }">
+  <div class="relative" ref="navDropdown">
     <button
-      x-on:click="open = !open"
+      @click.prevent="open = !open"
       class="flex flex-row items-center w-full rounded-full px-10 py-2 mt-2 md:text-lg text-sm border text-left bg-transparent md:w-auto md:inline md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
     >
       <span>{{ text }}</span>
     </button>
-    <div
-      x-show="open"
-      x-transition:enter="transition ease-out duration-100"
-      x-transition:enter-start="transform opacity-0 scale-95"
-      x-transition:enter-end="transform opacity-100 scale-100"
-      x-transition:leave="transition ease-in duration-75"
-      x-transition:leave-start="transform opacity-100 scale-100"
-      x-transition:leave-end="transform opacity-0 scale-95"
-      class="absolute right-0 w-full md:w-64 mt-2 origin-top-right rounded-md shadow-lg z-10"
-    >
-      <div class="bg-white rounded-md shadow">
-        <slot />
+    <transition>
+      <div
+        v-show="open"
+        class="absolute right-0 w-full md:w-64 mt-2 origin-top-right rounded-md shadow-lg z-10"
+      >
+        <div class="bg-white rounded-md shadow">
+          <slot />
+        </div>
       </div>
-    </div>
+    </transition>
   </div>
 </template>
 <script lang="ts">
 import { Vue, prop, Options } from "vue-class-component";
-import "alpinejs";
 
 class Props {
   text = prop<string>({ required: true });
@@ -33,5 +28,50 @@ class Props {
 @Options({
   name: "NavDropdown",
 })
-export default class NavDropdown extends Vue.with(Props) {}
+export default class NavDropdown extends Vue.with(Props) {
+  $refs!: {
+    navDropdown: HTMLFormElement;
+  };
+
+  open = false;
+
+  mounted(): void {
+    this.open = false;
+    document.body.addEventListener("click", this.handleClickAway);
+  }
+
+  unmounted(): void {
+    document.body.removeEventListener("click", this.handleClickAway);
+  }
+
+  handleClickAway(event: Event): void {
+    if (!this.$refs.navDropdown?.contains(event.target as HTMLElement)) {
+      this.open = false;
+    }
+    this.$router.beforeEach(() => {
+      this.open = false;
+    });
+  }
+}
 </script>
+
+<style lang="postcss" scoped>
+.v-enter-active {
+  @apply transition ease-out duration-100;
+}
+.v-enter-from {
+  @apply transform opacity-0 scale-95;
+}
+.v-enter-to {
+  @apply transform opacity-100 scale-100;
+}
+.v-leave-active {
+  @apply transition ease-in duration-75;
+}
+.v-leave-from {
+  @apply transform opacity-100 scale-100;
+}
+.v-leave-to {
+  @apply transform opacity-0 scale-95;
+}
+</style>
