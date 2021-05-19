aded5ff356bc1cc1f910a6f06a91dcb224f857de
Guillem Gelabert Sunyer <guillem.gelabert@liip.ch>
2021-05-19 16:28:52 +0200

    MATCHD-259: Remove unused alpinejs attributes

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,5 @@
 <template>
   <div
-    x-data="{ open: false }"
     :class="{ 'border-green-1': !isCompany, 'border-pink-1': isCompany }"
     class="flex flex-col md:items-center md:justify-between md:flex-row px-2.5 border-b py-2.5 bg-white"
   >
@@ -10,31 +9,10 @@
         class="text-lg tracking-widest text-gray-900 uppercase rounded-lg focus:outline-none focus:shadow-outline"
         ><Logo class="w-36"
       /></a>
-      <button
-        class="md:hidden rounded-lg focus:outline-none focus:shadow-outline"
-        x-on:click="open = !open"
-      >
-        <svg fill="currentColor" viewBox="0 0 20 20" class="w-6 h-6">
-          <path
-            x-show="!open"
-            fill-rule="evenodd"
-            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
-            clip-rule="evenodd"
-          ></path>
-          <path
-            x-show="open"
-            fill-rule="evenodd"
-            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-            clip-rule="evenodd"
-          ></path>
-        </svg>
-      </button>
+      <button class="md:hidden rounded-lg focus:outline-none focus:shadow-outline"></button>
     </div>
 
-    <nav
-      x-bind:class="{ flex: open, hidden: !open }"
-      class="flex-col flex-grow pb-4 md:pb-0 hidden md:flex md:justify-end md:flex-row"
-    >
+    <nav class="flex-col flex-grow pb-4 md:pb-0 hidden md:flex md:justify-end md:flex-row">
       <slot />
     </nav>
   </div>
