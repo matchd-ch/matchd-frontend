5f92e21880e02e908d403edb8ad7b543f036d514
Rico Schmidt <schmidt@joshmartin.ch>
2022-02-15 13:25:20 +0100

    T2496: Setup Google Tagmanager

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { createGtm } from "@gtm-support/vue-gtm";
 import { createApp } from "vue";
 import { createMetaManager } from "vue-meta";
 import App from "./App.vue";
@@ -9,4 +10,17 @@ const app = createApp(App);
 app.use(store);
 app.use(router);
 app.use(createMetaManager());
+
+// See https://www.npmjs.com/package/@gtm-support/vue-gtm
+app.use(
+  createGtm({
+    id: process.env.VUE_APP_GTM_CONTAINER_ID || null,
+    defer: false,
+    enabled: process.env.VUE_APP_GTM_ENABLED === "true",
+    debug: process.env.VUE_APP_GTM_DEBUG === "true",
+    loadScript: true,
+    vueRouter: router,
+  })
+);
+
 app.mount("#app");
