8133bd7b2bc2de16382fe151d45862b195cc59b2
Rico Schmidt <schmidt@joshmartin.ch>
2022-03-29 11:15:14 +0200

    chore: Add sentry

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { createGtm } from "@gtm-support/vue-gtm";
+import { BrowserTracing } from "@sentry/tracing";
+import * as Sentry from "@sentry/vue";
 import { createApp } from "vue";
 import { createMetaManager } from "vue-meta";
 import App from "./App.vue";
@@ -12,20 +14,20 @@ app.use(router);
 app.use(createMetaManager());
 
 // See https://docs.sentry.io/platforms/javascript/guides/vue/
-// if (process.env.VUE_APP_SENTRY_DSN && process.env.VUE_APP_SENTRY_ENVIRONMENT) {
-//   Sentry.init({
-//     app,
-//     dsn: process.env.VUE_APP_SENTRY_DSN,
-//     environment: process.env.VUE_APP_SENTRY_ENVIRONMENT,
-//     release: process.env.VUE_APP_SENTRY_RELEASE ? process.env.VUE_APP_SENTRY_RELEASE : undefined,
-//     integrations: [
-//       new BrowserTracing({
-//         routingInstrumentation: Sentry.vueRouterInstrumentation(router),
-//       }),
-//     ],
-//     tracesSampleRate: 1.0,
-//   });
-// }
+if (process.env.VUE_APP_SENTRY_DSN && process.env.VUE_APP_SENTRY_ENVIRONMENT) {
+  Sentry.init({
+    app,
+    dsn: process.env.VUE_APP_SENTRY_DSN,
+    environment: process.env.VUE_APP_SENTRY_ENVIRONMENT,
+    release: process.env.VUE_APP_SENTRY_RELEASE ? process.env.VUE_APP_SENTRY_RELEASE : undefined,
+    integrations: [
+      new BrowserTracing({
+        routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+      }),
+    ],
+    tracesSampleRate: 1.0,
+  });
+}
 
 // See https://www.npmjs.com/package/@gtm-support/vue-gtm
 if (process.env.VUE_APP_GTM_CONTAINER_ID) {
