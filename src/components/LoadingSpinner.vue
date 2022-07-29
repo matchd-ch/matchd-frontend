8dbb9303f8b652deb9350e0965bbf96a19953704
Rico Schmidt <schmidt@joshmartin.ch>
2022-07-29 15:14:30 +0200

    fix spinner size

--- src/components/LoadingSpinner.vue
+++ src/components/LoadingSpinner.vue
@@ -5,24 +5,17 @@
 </template>
 <script setup lang="ts">
 import LoadingSvg from "@/assets/icons/loading.svg";
-
-withDefaults(
-  defineProps<{
-    size?: string;
-  }>(),
-  {
-    size: "60px",
-  }
-);
 </script>
 <style lang="postcss" scoped>
 @block loading-spinner {
   & svg {
     @apply inline-block;
     color: var(--color-primary-1);
-    @apply animate-spin;
     fill: var(--color-primary-3);
     border-radius: 50%;
+    width: 60px;
+    height: 60px;
+    @apply animate-spin;
     transition: fill 0.4s cubic-bezier(0.4, 0, 0.2, 1);
   }
 }
