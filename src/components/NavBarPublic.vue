68f52e97a3e28c0bd047fdc46be074fe733b4312
Rico Schmidt <schmidt@joshmartin.ch>
2022-08-16 14:59:01 +0200

    Remove unused Navigation links

--- src/components/NavBarPublic.vue
+++ src/components/NavBarPublic.vue
@@ -2,8 +2,8 @@
   <NavBar :open="open" @toggle-navigation="onToggleNavigation">
     <div class="grow md:flex-row justify-center pb-4 md:pb-0 md:flex md:justify-center">
       <!-- <NavLink :to="{ name: 'Dashboard' }">Dashboard</NavLink> -->
-      <NavLink :to="{ name: 'StudentSearch' }">Talent finden</NavLink>
-      <NavLink :to="{ name: 'ProjectPostingSearch' }">Projektidee finden</NavLink>
+      <!-- <NavLink :to="{ name: 'StudentSearch' }">Talent finden</NavLink> -->
+      <!-- <NavLink :to="{ name: 'ProjectPostingSearch' }">Projektidee finden</NavLink> -->
     </div>
   </NavBar>
 </template>
@@ -11,7 +11,6 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import NavBar from "./NavBar.vue";
-import NavLink from "./NavLink.vue";
 
 const open = ref(false);
 const onToggleNavigation = () => {
