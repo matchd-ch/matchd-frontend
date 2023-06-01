3591c87de656c3eb1f72fad6c03fc5b48fb917b6
Rico Schmidt <schmidt@joshmartin.ch>
2023-06-01 16:58:33 +0200

    T4216: Update loader component & add LoadingBox

--- src/views/ChallengeDetail.vue
+++ src/views/ChallengeDetail.vue
@@ -1,183 +1,195 @@
 <template>
-  <div v-if="challenge.data" class="challenge-detail flex flex-col min-h-content-with-fixed-bars">
-    <div class="border-b border-orange-1 p-9 challenge-navigation-bar">
-      <div>
-        <button class="text-black hover:text-orange-1 transition-colors" @click="router.back()">
-          <ArrowBack class="w-5 mr-2 xl:mr-1 mb-1 shrink-0 inline-block" />Zurück zur Übersicht
-        </button>
+  <LoadingBox :is-loading="!!!challenge.data">
+    <div v-if="challenge.data" class="challenge-detail flex flex-col min-h-content-with-fixed-bars">
+      <div class="border-b border-orange-1 p-9 challenge-navigation-bar">
+        <div>
+          <button class="text-black hover:text-orange-1 transition-colors" @click="router.back()">
+            <ArrowBack class="w-5 mr-2 xl:mr-1 mb-1 shrink-0 inline-block" />Zurück zur Übersicht
+          </button>
+        </div>
+        <div v-if="getStepName(1)">
+          <DeleteChallenge :challenge="challenge.data" />
+        </div>
       </div>
-      <div v-if="getStepName(1)">
-        <DeleteChallenge :challenge="challenge.data" />
-      </div>
-    </div>
-    <PostingSection
-      :edit-step="getStepName(1)"
-      title="Challenge Beschreibung"
-      :slug="challenge.data.slug"
-    >
-      <h1 class="text-heading-sm">{{ challenge.data.displayTitle }}</h1>
-      <p class="mt-4">{{ challenge.data.challengeType.name }}</p>
-    </PostingSection>
-    <!-- Details zur Challenge -->
-    <PostingSection
-      title="Details zur Challenge"
-      :edit-step="getStepName(1)"
-      :slug="challenge.data.slug"
-    >
-      <h6 class="font-medium text-orange-1">Beschreibung</h6>
-      <!-- eslint-disable-next-line vue/no-v-html -->
-      <p class="mb-4 pr-10" v-html="nl2br(challenge.data.description)"></p>
-      <template v-if="isLoggedIn">
-        <template v-if="challenge.data.teamSize">
-          <h6 class="font-medium text-orange-1">Teamgrösse</h6>
-          <p class="mb-4">{{ getTeamSize(challenge.data.teamSize).label }}</p>
-        </template>
-        <template v-if="challenge.data.compensation">
-          <h6 class="font-medium text-orange-1">Vergütung</h6>
-          <p class="mb-4">{{ challenge.data.compensation }}</p>
-        </template>
-      </template>
-      <h6 class="font-medium text-orange-1 mb-2">Stichwörter</h6>
-      <SelectPillGroup class="mb-4">
-        <SelectPill v-for="keyword in challenge.data.keywords" :key="keyword.id" :has-input="false">
-          {{ keyword.name }}
-        </SelectPill>
-      </SelectPillGroup>
-    </PostingSection>
-
-    <!-- TODO: Check if min one element set -->
-    <PostingSection
-      title="Weitere Informationen"
-      :edit-step="getStepName(2)"
-      :slug="challenge.data.slug"
-    >
-      <template v-if="challenge.data.challengeFromDate">
-        <h6 class="font-medium text-orange-1">Starttermin</h6>
-        <p class="mb-4">{{ formatDate(challenge.data.challengeFromDate, "LLLL yyyy") }}</p>
-      </template>
-      <template v-if="isLoggedIn">
-        <template v-if="challenge.documents.length">
-          <h6 class="font-medium text-orange-1">Dokumente</h6>
-          <ul class="list list-inside mb-4">
-            <li v-for="document in challenge.documents" :key="document.id">
-              <a
-                :href="replaceStack(document.url, '')"
-                target="_blank"
-                download
-                class="underline hover:text-orange-1 transition-colors"
-              >
-                {{ document.fileName }}
-              </a>
-            </li>
-          </ul>
-        </template>
-        <template v-if="challenge.images.length">
-          <h6 class="font-medium text-orange-1">Bilder</h6>
-          <ul class="list list-inside mb-4">
-            <li v-for="image in challenge.images" :key="image.id">
-              <a
-                :href="replaceStack(image.url, 'image-large')"
-                target="_blank"
-                class="underline hover:text-orange-1 transition-colors"
-              >
-                {{ image.fileName }}
-              </a>
-            </li>
-          </ul>
-        </template>
-        <p
-          v-if="challenge.data.website"
-          class="flex items-center mt-4 text-black hover:text-orange-1 transition-colors"
-        >
-          <span class="material-icons mr-2">open_in_new</span>
-          <a :href="challenge.data.website" target="_blank" class="underline">Website</a>
-        </p>
-      </template>
-    </PostingSection>
-    <!-- Neugierig -->
-    <PostingSection v-if="isLoggedIn" title="Neugierig? Dann lassen wir es matchen!">
-      <template v-if="challenge.data.company">
-        <router-link :to="{ name: 'CompanyDetail', params: { slug: challenge.data.company.slug } }">
-          <address class="not-italic text-black hover:text-orange-1 transition-colors flex text-lg">
-            <div>
-              <h3 class="text-heading-sm mb-3">{{ challenge.data.company?.name }}</h3>
-              {{ challenge.data.company?.street }}
-              <br />
-              {{ challenge.data.company?.zip }}
-              {{ challenge.data.company?.city }}
-            </div>
-            <IconArrow class="w-8 ml-8" />
-          </address>
-          <h3 class="block text-heading-sm mb-3 mt-4">
-            {{ challenge.data.employee?.firstName }}
-            {{ challenge.data.employee?.lastName }}
-          </h3>
-          <p>
-            <span>{{ challenge.data.employee?.role }}&nbsp;</span>
-            <a
-              :href="`mailto:${challenge.data.employee?.email}`"
-              target="_blank"
-              class="underline font-medium text-black hover:text-orange-1 transition-colors"
-            >
-              {{ challenge.data.employee?.email }}
-            </a>
-          </p>
-        </router-link>
-      </template>
-      <template v-else>
-        <router-link
-          :to="{ name: 'StudentDetail', params: { slug: challenge.data.student?.slug } }"
-          class="underline font-medium text-black hover:text-orange-1 transition-colors"
-        >
-          <h3 class="text-heading-sm flex">
-            <template v-if="challenge.data.student?.firstName && challenge.data.student?.lastName">
-              {{ challenge.data.student?.firstName }}
-              {{ challenge.data.student?.lastName }}
-            </template>
-            <template v-else>{{ challenge.data.student?.nickname }}</template>
-            <IconArrow class="w-8 ml-8" />
-          </h3>
-        </router-link>
-      </template>
-    </PostingSection>
-    <teleport to="footer">
-      <MatchingBar
-        v-if="
-          isLoggedIn &&
-          ((challenge.data.student && !isStudent) || (challenge.data.company && isStudent))
-        "
+      <PostingSection
+        :edit-step="getStepName(1)"
+        title="Challenge Beschreibung"
+        :slug="challenge.data.slug"
       >
-        <template v-if="matchType === matchTypeEnum.FullMatch">
-          <template v-if="isStudent">Du hast dein Interesse gezeigt!</template>
-          <template v-else>Sie haben Ihr Interesse gezeigt!</template>
+        <h1 class="text-heading-sm">{{ challenge.data.displayTitle }}</h1>
+        <p class="mt-4">{{ challenge.data.challengeType.name }}</p>
+      </PostingSection>
+      <!-- Details zur Challenge -->
+      <PostingSection
+        title="Details zur Challenge"
+        :edit-step="getStepName(1)"
+        :slug="challenge.data.slug"
+      >
+        <h6 class="font-medium text-orange-1">Beschreibung</h6>
+        <!-- eslint-disable-next-line vue/no-v-html -->
+        <p class="mb-4 pr-10" v-html="nl2br(challenge.data.description)"></p>
+        <template v-if="isLoggedIn">
+          <template v-if="challenge.data.teamSize">
+            <h6 class="font-medium text-orange-1">Teamgrösse</h6>
+            <p class="mb-4">{{ getTeamSize(challenge.data.teamSize).label }}</p>
+          </template>
+          <template v-if="challenge.data.compensation">
+            <h6 class="font-medium text-orange-1">Vergütung</h6>
+            <p class="mb-4">{{ challenge.data.compensation }}</p>
+          </template>
         </template>
-        <MatchdButton v-else @click="onClickMatch">Mit dieser Challenge matchen</MatchdButton>
-      </MatchingBar>
-      <MatchingBar v-else-if="!isLoggedIn" @click="router.push({ name: 'Triage' })">
-        <MatchdButton>Neugierig?</MatchdButton>
-      </MatchingBar>
-    </teleport>
-    <template v-if="user">
-      <ChallengeCompanyMatchModal
-        v-if="challenge.data && showConfirmationModal && isStudent"
-        :user="user"
-        :challenge="challenge.data"
-        :loading="matchLoading"
-        :match-type="matchType"
-        @click-confirm="onClickMatchConfirm"
-        @click-cancel="onClickCancel"
-      />
-      <ChallengeStudentMatchModal
-        v-if="showConfirmationModal && !isStudent"
-        :user="user"
-        :challenge="challenge.data"
-        :loading="matchLoading"
-        :match-type="matchType"
-        @click-confirm="onClickMatchConfirm"
-        @click-cancel="onClickCancel"
-      />
-    </template>
-  </div>
+        <h6 class="font-medium text-orange-1 mb-2">Stichwörter</h6>
+        <SelectPillGroup class="mb-4">
+          <SelectPill
+            v-for="keyword in challenge.data.keywords"
+            :key="keyword.id"
+            :has-input="false"
+          >
+            {{ keyword.name }}
+          </SelectPill>
+        </SelectPillGroup>
+      </PostingSection>
+
+      <!-- TODO: Check if min one element set -->
+      <PostingSection
+        title="Weitere Informationen"
+        :edit-step="getStepName(2)"
+        :slug="challenge.data.slug"
+      >
+        <template v-if="challenge.data.challengeFromDate">
+          <h6 class="font-medium text-orange-1">Starttermin</h6>
+          <p class="mb-4">{{ formatDate(challenge.data.challengeFromDate, "LLLL yyyy") }}</p>
+        </template>
+        <template v-if="isLoggedIn">
+          <template v-if="challenge.documents.length">
+            <h6 class="font-medium text-orange-1">Dokumente</h6>
+            <ul class="list list-inside mb-4">
+              <li v-for="document in challenge.documents" :key="document.id">
+                <a
+                  :href="replaceStack(document.url, '')"
+                  target="_blank"
+                  download
+                  class="underline hover:text-orange-1 transition-colors"
+                >
+                  {{ document.fileName }}
+                </a>
+              </li>
+            </ul>
+          </template>
+          <template v-if="challenge.images.length">
+            <h6 class="font-medium text-orange-1">Bilder</h6>
+            <ul class="list list-inside mb-4">
+              <li v-for="image in challenge.images" :key="image.id">
+                <a
+                  :href="replaceStack(image.url, 'image-large')"
+                  target="_blank"
+                  class="underline hover:text-orange-1 transition-colors"
+                >
+                  {{ image.fileName }}
+                </a>
+              </li>
+            </ul>
+          </template>
+          <p
+            v-if="challenge.data.website"
+            class="flex items-center mt-4 text-black hover:text-orange-1 transition-colors"
+          >
+            <span class="material-icons mr-2">open_in_new</span>
+            <a :href="challenge.data.website" target="_blank" class="underline">Website</a>
+          </p>
+        </template>
+      </PostingSection>
+      <!-- Neugierig -->
+      <PostingSection v-if="isLoggedIn" title="Neugierig? Dann lassen wir es matchen!">
+        <template v-if="challenge.data.company">
+          <router-link
+            :to="{ name: 'CompanyDetail', params: { slug: challenge.data.company.slug } }"
+          >
+            <address
+              class="not-italic text-black hover:text-orange-1 transition-colors flex text-lg"
+            >
+              <div>
+                <h3 class="text-heading-sm mb-3">{{ challenge.data.company?.name }}</h3>
+                {{ challenge.data.company?.street }}
+                <br />
+                {{ challenge.data.company?.zip }}
+                {{ challenge.data.company?.city }}
+              </div>
+              <IconArrow class="w-8 ml-8" />
+            </address>
+            <h3 class="block text-heading-sm mb-3 mt-4">
+              {{ challenge.data.employee?.firstName }}
+              {{ challenge.data.employee?.lastName }}
+            </h3>
+            <p>
+              <span>{{ challenge.data.employee?.role }}&nbsp;</span>
+              <a
+                :href="`mailto:${challenge.data.employee?.email}`"
+                target="_blank"
+                class="underline font-medium text-black hover:text-orange-1 transition-colors"
+              >
+                {{ challenge.data.employee?.email }}
+              </a>
+            </p>
+          </router-link>
+        </template>
+        <template v-else>
+          <router-link
+            :to="{ name: 'StudentDetail', params: { slug: challenge.data.student?.slug } }"
+            class="underline font-medium text-black hover:text-orange-1 transition-colors"
+          >
+            <h3 class="text-heading-sm flex">
+              <template
+                v-if="challenge.data.student?.firstName && challenge.data.student?.lastName"
+              >
+                {{ challenge.data.student?.firstName }}
+                {{ challenge.data.student?.lastName }}
+              </template>
+              <template v-else>{{ challenge.data.student?.nickname }}</template>
+              <IconArrow class="w-8 ml-8" />
+            </h3>
+          </router-link>
+        </template>
+      </PostingSection>
+      <teleport to="footer">
+        <MatchingBar
+          v-if="
+            isLoggedIn &&
+            ((challenge.data.student && !isStudent) || (challenge.data.company && isStudent))
+          "
+        >
+          <template v-if="matchType === matchTypeEnum.FullMatch">
+            <template v-if="isStudent">Du hast dein Interesse gezeigt!</template>
+            <template v-else>Sie haben Ihr Interesse gezeigt!</template>
+          </template>
+          <MatchdButton v-else @click="onClickMatch">Mit dieser Challenge matchen</MatchdButton>
+        </MatchingBar>
+        <MatchingBar v-else-if="!isLoggedIn" @click="router.push({ name: 'Triage' })">
+          <MatchdButton>Neugierig?</MatchdButton>
+        </MatchingBar>
+      </teleport>
+      <template v-if="user">
+        <ChallengeCompanyMatchModal
+          v-if="challenge.data && showConfirmationModal && isStudent"
+          :user="user"
+          :challenge="challenge.data"
+          :loading="matchLoading"
+          :match-type="matchType"
+          @click-confirm="onClickMatchConfirm"
+          @click-cancel="onClickCancel"
+        />
+        <ChallengeStudentMatchModal
+          v-if="showConfirmationModal && !isStudent"
+          :user="user"
+          :challenge="challenge.data"
+          :loading="matchLoading"
+          :match-type="matchType"
+          @click-confirm="onClickMatchConfirm"
+          @click-cancel="onClickCancel"
+        />
+      </template>
+    </div>
+  </LoadingBox>
 </template>
 
 <script setup lang="ts">
@@ -185,6 +197,7 @@ import { ProfileType } from "@/api/models/types";
 import ArrowBack from "@/assets/icons/arrow-back.svg";
 import IconArrow from "@/assets/icons/arrow.svg";
 import DeleteChallenge from "@/components/DeleteChallenge.vue";
+import LoadingBox from "@/components/LoadingBox.vue";
 import MatchdButton from "@/components/MatchdButton.vue";
 import MatchingBar from "@/components/MatchingBar.vue";
 import ChallengeCompanyMatchModal from "@/components/modals/ChallengeCompanyMatchModal.vue";
--- src/views/DashboardOverview.vue
+++ src/views/DashboardOverview.vue
@@ -1,27 +1,31 @@
 <template>
-  <template v-if="user">
-    <StudentDashboard
-      v-if="isStudent && studentDashboard"
-      :dashboard="studentDashboard"
-      class="min-h-content-with-fixed-bars"
-    />
-    <CompanyDashboard
-      v-else-if="companyDashboard"
-      :dashboard="companyDashboard"
-      class="min-h-content-with-fixed-bars"
-    />
-  </template>
+  <LoadingBox class="loading-box" :is-loading="loadingState.isLoading.value">
+    <template v-if="user">
+      <StudentDashboard
+        v-if="isStudent && studentDashboard"
+        :dashboard="studentDashboard"
+        class="min-h-content-with-fixed-bars"
+      />
+      <CompanyDashboard
+        v-else-if="companyDashboard"
+        :dashboard="companyDashboard"
+        class="min-h-content-with-fixed-bars"
+      />
+    </template>
+  </LoadingBox>
 </template>
 
 <script setup lang="ts">
 import { AttachmentKey } from "@/api/models/types";
+import LoadingBox from "@/components/LoadingBox.vue";
 import CompanyDashboard from "@/components/dashboard/CompanyDashboard.vue";
 import StudentDashboard from "@/components/dashboard/StudentDashboard.vue";
+import useLoadingState from "@/composables/useLoadingState";
 import { calculateMargins } from "@/helpers/calculateMargins";
 import { useStore } from "@/store";
 import { ActionTypes as ContentActions } from "@/store/modules/content/action-types";
 import { ActionTypes as UploadActionTypes } from "@/store/modules/upload/action-types";
-import { computed, onMounted } from "vue";
+import { computed, onMounted, watchEffect } from "vue";
 import { setup } from "vue-class-component";
 import { useMeta } from "vue-meta";
 
@@ -37,6 +41,21 @@ const studentDashboard = computed(() => store.getters["dashboard"]);
 const companyDashboard = computed(() => store.getters["companyDashboard"]);
 const isStudent = computed(() => store.getters["isStudent"]);
 const user = computed(() => store.getters["user"]);
+
+const loadingState = useLoadingState<{ user: boolean; dashboard: boolean }>({
+  user: !!user.value,
+  dashboard: true,
+});
+
+watchEffect(() => {
+  if (isStudent.value) {
+    loadingState.set("dashboard", !!!studentDashboard.value);
+  } else {
+    loadingState.set("dashboard", !!!companyDashboard.value);
+  }
+  loadingState.set("user", !!!user.value);
+  console.log(loadingState.loadingState.value);
+});
 
 onMounted(async () => {
   await Promise.all([
@@ -54,4 +73,8 @@ onMounted(async () => {
 });
 </script>
 
-<style></style>
+<style lang="postcss" scoped>
+.loading-box {
+  height: 100%;
+}
+</style>
