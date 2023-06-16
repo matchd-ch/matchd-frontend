ab69d9a12e1006559304a28067b1373bde06bc79
Rico Schmidt <reeko@users.noreply.github.com>
2023-06-16 14:53:37 +0200

    T4054: Add Stellenantritt => nach Vereinbarung (#1180)
    * T4054: Add Stellenantritt => nach Vereinbarung
    
    * U(pdate backend error messages
    
    * T4054: Remove jobPeriodByAgreement
    
    * T4054: Update jobposting detail dateFrom dateTo

--- types/api.d.ts
+++ types/api.d.ts
@@ -660,7 +660,6 @@ type JobPosting = Node & {
   /** The ID of the object */
   id: Scalars["ID"];
   jobFromDate?: Maybe<Scalars["Date"]>;
-  jobPeriodByAgreement: Scalars["Boolean"];
   jobRequirements: JobRequirementConnection;
   jobToDate?: Maybe<Scalars["Date"]>;
   jobType: JobType;
@@ -709,7 +708,6 @@ type JobPostingBaseDataInput = {
   description?: InputMaybe<Scalars["String"]>;
   id?: InputMaybe<Scalars["String"]>;
   jobFromDate?: InputMaybe<Scalars["String"]>;
-  jobPeriodByAgreement: Scalars["Boolean"];
   jobToDate?: InputMaybe<Scalars["String"]>;
   jobType: JobTypeInput;
   /** Title */
